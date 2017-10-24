8ec71f5a18d28f0ee1dbbef9010f1c2d603ce3c4
seif <em_lakehal@esi.dz>
2017-10-24 17:47:28 +0100

    le main de ce tp est ajouté

---
+++ notretp/NotreTp.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package notretp;
+
+import static notretp.VectorHelper.gestion_menu;
+import java.io.IOException;
+
+/**
+ *
+ * @author DELL XPS
+ */
+public class NotreTp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException, Tai_Dif_Exception {
+        gestion_menu();
+    }
+    
+}
+
+
